f991f389fd247ba191ee5fe09a198933cd5baf77
byeori-moon <mblee2@naver.com>
2024-02-07 12:39:24 +0900

    merge

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:get/get.dart';
