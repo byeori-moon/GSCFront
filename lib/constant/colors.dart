b3362eee90af104cb5f980643fe1580d3ff1859a
byeori-moon <mblee2@naver.com>
2024-02-20 22:22:55 +0900

    bottombar 구현완료

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const Color BACKGROUND_COLOR=Color(0XFFF6FCFF);
 const Color BACKGROUND_SECOND_COLOR=Color(0XFFFFF9F7);
 const Color MAINSCREEN_COLOR=Color(0XFFEDFFF8);
+const Color FIRSTSCREEN_COLOR=Color(0XFF6287AA);
 const Color DEFAULT_YELLOW=Color(0XFFFDFFEF);
 const Color BUTTON_BLUE=Color(0XFF97ACB8);
 const Color BUTTON_WHITE=Color(0XFFFEFDF8);
---
+++ lib/screen/quiz_main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:camera_pj/constant/colors.dart';
+import 'package:flutter/material.dart';
+
+class QuizMainScreen extends StatelessWidget {
+  const QuizMainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: FIRSTSCREEN_COLOR,
+    );
+  }
+}
